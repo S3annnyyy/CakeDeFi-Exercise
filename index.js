593b940b0ce0939366dc134f55477d8c82d449e9
Sean Yap <seanyckang@gmail.com>
2023-03-02 23:39:00 +0800

    Remove roundDown & updated getTokenSale function

--- index.js
+++ index.js
@@ -10,46 +10,25 @@ let RATE = new Map();
 const CURRENCY = ["BTC", "ETH", "DOGE"];
 
 // Initialize token sale function
-function MathRound(num, nrdecimals) {
-    let n = num.toFixed(nrdecimals);
-    return (n > num) ? n-(1/(Math.pow(10,nrdecimals))) : n;
-}
-
 function getTokenSale(ethSaleRate, deci, purchCurr, purchAmt) {
-    deci_place = parseInt(deci);
-    BigNumber.set({ DECIMAL_PLACES: deci_place });
-    if (purchCurr === "ETH") {
-        if (ethSaleRate.length > 10 || purchAmt.length > 10) {
-            x = new BigNumber(ethSaleRate);
-            y = new BigNumber(purchAmt);
-            z = y.multipliedBy(x).decimalPlaces(deci_place).toFixed(deci_place);
-        } else {
-            test = parseFloat(ethSaleRate*purchAmt);
-            z = MathRound(test, deci_place).toString();
-        }
-        } else {
-            if (ethSaleRate.length > 10 || purchAmt.length > 10) {
-                x = new BigNumber(ethSaleRate);
-                y = new BigNumber(purchAmt * (RATE.get(purchCurr) / RATE.get("ETH")));
-                z = y.multipliedBy(x).decimalPlaces(deci_place).toFixed(deci_place);
-            } else {
-                test = parseFloat(ethSaleRate*(purchAmt* (RATE.get(purchCurr) / RATE.get("ETH"))));
-                z = MathRound(test, deci_place).toString();
-            }
-        }
-        return z;
+    BigNumber.set({ DECIMAL_PLACES: deci, ROUNDING_MODE: BigNumber.ROUND_DOWN });
+
+    ethSR = BigNumber(ethSaleRate);
+    cVR = RATE.get(purchCurr) / RATE.get("ETH");
+    tmp = BigNumber(purchAmt).multipliedBy(cVR);
+    final = tmp.multipliedBy(ethSR).decimalPlaces(deci).toFixed(deci);
+    return final;
 }
 
 rl.on('line', (line) => {
     line = line.split(" ");
     if (line.length === 3) {
-        // store values in Map
+        // store values in RATE Map
         for (let i=0; i<line.length; i++) {
             RATE.set(CURRENCY[i], line[i]);
         }
-        // console.log(RATE);
-    } else {
-        const output = getTokenSale.apply(this, line);
+    } else if (line.length === 4) {
+        const output = getTokenSale(line[0], parseInt(line[1]), line[2], line[3]);
         console.log(output);
     }
 });
